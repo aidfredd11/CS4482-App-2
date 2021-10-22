39ab6aefa45c2abcce2114f9b6d6e1d21a960bf6
Aidan <aidfrederick11@gmail.com>
2021-10-22 15:47:35 -0400

    No more empty entries in leaderboard
    Still working on formatting text

--- Maze-Game/Assets/Scripts/Leaderboard.cs
+++ Maze-Game/Assets/Scripts/Leaderboard.cs
@@ -3,52 +3,86 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // get time and name
-        float recordedScore = PlayerPrefs.GetFloat("timeScore");
-        string name = PlayerPrefs.GetString("name");
-      
-        // Clear the player prefs 
-        PlayerPrefs.DeleteAll();
-
-        // Load the previous data
-        LeaderBoardData loadedData = DataSaver.loadData<LeaderBoardData>("leaderboard");
-        loadedData.names.Add(name);
-        loadedData.times.Add(recordedScore);
-        DataSaver.saveData(loadedData, "leaderboard");
-
-        // Instantiate data object
-        //LeaderBoardData saveData = new LeaderBoardData();
-
-        // Add current combo to list 
-       // saveData.names.Add(name);
-       // saveData.times.Add(recordedScore);
-
-        // Save it
-      //  DataSaver.saveData(saveData, "leaderboard");
-
-        // Get data now with new score
-        LeaderBoardData leaderBoardData = DataSaver.loadData<LeaderBoardData>("leaderboard");
-
-        if (leaderBoardData == null)
+        // only add a new score if the player pref is not empty
+        if(PlayerPrefs.HasKey("timeScore") && PlayerPrefs.HasKey("name"))
         {
-            Debug.Log("Empty leaderboard");
+
+            // get time and name
+            float recordedScore = PlayerPrefs.GetFloat("timeScore");
+            string name = PlayerPrefs.GetString("name");
+
+            // Clear the player prefs 
+            PlayerPrefs.DeleteAll();
+
+            // Load the previous data
+            LeaderBoardData loadedData = DataSaver.loadData<LeaderBoardData>("leaderboard");
+
+            // add the score and name to the data
+            loadedData.names.Add(name);
+            loadedData.times.Add(recordedScore);
+
+            // Save it
+            DataSaver.saveData(loadedData, "leaderboard");
+
+            GenerateTextObjects(loadedData);
         }
+        else // leaderboard is opened from the main menu
+        {
+            LeaderBoardData loadedData = DataSaver.loadData<LeaderBoardData>("leaderboard");
+            if (loadedData == null)
+            {
+                Debug.Log("Empty leaderboard");
+            }
+
+            GenerateTextObjects(loadedData);
+        }    
+
+    }
+
+    private void GenerateTextObjects(LeaderBoardData data)
+    {
+        
+        for (int i =0; i < data.names.Count; i++)
+        {
+            // get the current item
+            string thisName = data.names[i];
+            string thisTime = data.times[i].ToString();
+            // combine to one string
+            string entryString = thisName + "_____" + thisTime;
+
+            // make a gameobject for the text
+            GameObject newText = new GameObject("Entry", typeof(RectTransform));
+            newText.layer = 5; // make it a ui layer
+
+            // Change its size
+            RectTransform rectTransform = newText.GetComponent<RectTransform>();
+            float temp = rectTransform.localScale.x;
+            rectTransform.localScale = new Vector3(1/temp, 1, 1);
+            rectTransform.sizeDelta = new Vector2(344, 66);
+
+            // add a text componet to new game object
+            var newTextComp = newText.AddComponent<Text>();
+
+            // set up text
+            newTextComp.text = entryString;
+            newTextComp.alignment = TextAnchor.MiddleCenter;
+            newTextComp.color = Color.black;
             
-        for (int i = 0; i < leaderBoardData.names.Count; i++)
-        {
-            Debug.Log(leaderBoardData.names[i]);
-        }
-        for (int i = 0; i < leaderBoardData.times.Count; i++)
-        {
-            Debug.Log(leaderBoardData.times[i]);
-        }
+            Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+            newTextComp.font = ArialFont;
+            newTextComp.fontSize = 22;
 
+            // make it a child of content gameobject
+            newText.transform.SetParent(transform);
+                     
+        }
     }
 
 }
